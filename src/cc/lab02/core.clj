efe38a9f277999005d175fb00a51e45a1bf2072e
Никита Иксарица <xomav2@gmail.com>
2021-04-13 23:12:41 +0300

    Добавлена функция для парсинга грамматики из xml

--- src/cc/lab02/core.clj
+++ src/cc/lab02/core.clj
@@ -1,5 +1,8 @@
 (ns cc.lab02.core
+  (:require [cc.lab02.helpers :refer [xml->map]])
   (:gen-class))
+
+(xml->map "./resources/grammar.xml")
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/cc/lab02/helpers.clj
@@ -0,0 +1,37 @@
+(ns cc.lab02.helpers
+  (:require [clojure.xml :as xml]
+            [clojure.data.zip.xml :refer [xml-> xml1->]]
+            [clojure.zip :as zip]))
+
+(defn xml->map [path]
+  (let [grammar (zip/xml-zip (xml/parse path))
+        terms (->> (xml-> grammar :grammar :terminalsymbols :term)
+                   (map zip/node)
+                   (map :attrs)
+                   (set))
+        nonterms (->> (xml-> grammar :grammar :nonterminalsymbols :nonterm)
+                      (map zip/node)
+                      (map :attrs)
+                      (set))
+        prods (-> grammar
+                  (xml-> :grammar :productions :production)
+                  (->> (map zip/node)))
+        lhs (->> prods
+                 (map zip/xml-zip)
+                 (map #(xml1-> % :production :lhs))
+                 (map zip/node)
+                 (map :attrs))
+        rhs (->> prods
+                 (map zip/xml-zip)
+                 (map #(xml-> % :production :rhs :symbol))
+                 (map #(map zip/node %))
+                 (map #(map :attrs %))
+                 (map vec))
+        prods (map #(hash-map :lhs % :rhs %2) lhs rhs)
+        start-symbol (->> (xml1-> grammar :grammar :startsymbol)
+                          (zip/node)
+                          (:attrs))]
+    {:terms terms
+     :nonterms nonterms
+     :prods prods
+     :start-symbol start-symbol}))
