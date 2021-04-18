b042fb56d288dd9b8709c8b862cbfc75312afae2
Никита Иксарица <xomav2@gmail.com>
2021-04-18 14:43:04 +0300

    Функция для чтения xml вынесена в отдельный файл

--- src/cc/lab02/helpers.clj
+++ src/cc/lab02/helpers.clj
@@ -1,8 +1,5 @@
 (ns cc.lab02.helpers
-  (:require #_[clojure.xml :as xml]
-            #_[clojure.data.zip.xml :refer [xml-> xml1->]]
-            #_[clojure.zip :as zip]
-            [clojure.data.json :as json]
+  (:require [clojure.data.json :as json]
             [camel-snake-kebab.core :as csk]))
 
 (defn json->grammar [path]
@@ -19,36 +16,3 @@
   (let [json-string (json/write-str grammar
                                     :key-fn csk/->camelCaseString)]
     (spit path json-string)))
-
-#_(defn xml->grammar [path]
-  (let [grammar (zip/xml-zip (xml/parse path))
-        terms (->> (xml-> grammar :grammar :terminalsymbols :term)
-                   (map zip/node)
-                   (map :attrs)
-                   (set))
-        nonterms (->> (xml-> grammar :grammar :nonterminalsymbols :nonterm)
-                      (map zip/node)
-                      (map :attrs)
-                      (set))
-        prods (-> grammar
-                  (xml-> :grammar :productions :production)
-                  (->> (map zip/node)))
-        lhs (->> prods
-                 (map zip/xml-zip)
-                 (map #(xml1-> % :production :lhs))
-                 (map zip/node)
-                 (map :attrs))
-        rhs (->> prods
-                 (map zip/xml-zip)
-                 (map #(xml-> % :production :rhs :symbol))
-                 (map #(map zip/node %))
-                 (map #(map :attrs %))
-                 (map vec))
-        prods (map #(hash-map :lhs % :rhs %2) lhs rhs)
-        start-symbol (->> (xml1-> grammar :grammar :startsymbol)
-                          (zip/node)
-                          (:attrs))]
-    {:terms terms
-     :nonterms nonterms
-     :prods prods
-     :start-symbol start-symbol}))
