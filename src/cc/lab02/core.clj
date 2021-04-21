dd6348881a38629d29643e4cbe1f4a2ce343eeae
Nikki <iksaritsaNI@tsniimash.ru>
2021-04-21 15:56:26 +0300

    Написан main

--- src/cc/lab02/core.clj
+++ src/cc/lab02/core.clj
@@ -1,10 +1,17 @@
 (ns cc.lab02.core
-  (:require [cc.lab02.helpers :refer [xml->map]])
+  (:require [cc.lab02.helpers :refer [json->grammar grammar->json]]
+            [cc.lab02.morph :as morph])
   (:gen-class))
-
-(xml->map "./resources/grammar.xml")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [g-0 (json->grammar "./resources/grammar.json")
+        g-0 (morph/remove-left-recursion g-0)
+        _ (grammar->json "./resources/grammar-result.json" g-0)
+
+        g-eps (json->grammar "./resources/grammar-eps.json")
+        g-eps (morph/remove-epsilon-rules g-eps)
+        _ (grammar->json "./resources/grammar-eps-result.json" g-eps)]))
+
+#_(-main)
