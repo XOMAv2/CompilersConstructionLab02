79ccbfbb23374d1a95c605eb9431ed75614bacca
Никита Иксарица <xomav2@gmail.com>
2021-04-23 17:38:20 +0300

    Слегка изменённый main

--- src/cc/lab02/core.clj
+++ src/cc/lab02/core.clj
@@ -1,17 +1,18 @@
 (ns cc.lab02.core
   (:require [cc.lab02.helpers :refer [json->grammar grammar->json]]
-            [cc.lab02.morph :as morph])
+            [cc.lab02.morph :as morph]
+            [clojure.string :as s])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (let [g-0 (json->grammar "./resources/grammar.json")
-        g-0 (morph/remove-left-recursion g-0)
-        _ (grammar->json "./resources/grammar-result.json" g-0)
+  [g-left-path g-eps-path]
+  (let [g-left (json->grammar g-left-path)
+        g-left (morph/remove-left-recursion g-left)
+        _ (grammar->json (s/replace g-left-path #".json$" "-result.json") g-left)
 
-        g-eps (json->grammar "./resources/grammar-eps.json")
+        g-eps (json->grammar g-eps-path)
         g-eps (morph/remove-epsilon-rules g-eps)
-        _ (grammar->json "./resources/grammar-eps-result.json" g-eps)]))
+        _ (grammar->json (s/replace g-eps-path #".json$" "-result.json") g-eps)]))
 
-#_(-main)
+#_(-main "./resources/grammar.json"
+         "./resources/grammar-c.json")
